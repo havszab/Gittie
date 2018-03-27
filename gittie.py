17e4a589049650fed4b62f43fa16fe920cdd02fe
havszab <havaldaszab@gmail.com>
2018-03-27 09:43:35 +0200

    add function added

--- gittie.py
+++ gittie.py
@@ -1,2 +1,8 @@
 def introduce():
     print("Hello, I'm Gittie!")
+
+def add():
+    first_num = input(float('Enter a number: '))
+    second_num = input(float('Enter a second number: '))
+    print(first_num + second_num)
+
