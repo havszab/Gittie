af0b1ebf565566491e77636f473916bea19756e4
havszab <havaldaszab@gmail.com>
2018-03-27 09:53:14 +0200

    introduce changed

--- gittie.py
+++ gittie.py
@@ -1,13 +1,11 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Hello, I'm Gittie, what\'s your name?")
 
-<<<<<<< HEAD
 def joke():
     print("I own the world’s worst thesaurus. Not only is it awful, it’s awful.")
-=======
+
 def add():
     first_num = input(float('Enter a number: '))
     second_num = input(float('Enter a second number: '))
     print(first_num + second_num)
 
->>>>>>> 17e4a589049650fed4b62f43fa16fe920cdd02fe
